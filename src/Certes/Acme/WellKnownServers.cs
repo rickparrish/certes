31457f2c4643ef0f6f4f5925078e12b4a2f052b5
rickparrish <rickparrish@users.noreply.github.com>
2023-03-05 09:19:32 -0500

    Add Google and ZeroSSL

--- src/Certes/Acme/WellKnownServers.cs
+++ src/Certes/Acme/WellKnownServers.cs
@@ -8,6 +8,22 @@ namespace Certes.Acme
     public class WellKnownServers
     {
         /// <summary>
+        /// Gets the URI for Google ACME v2 production server.
+        /// </summary>
+        /// <value>
+        /// The URI for Google ACME v2 production server.
+        /// </value>
+        public static Uri GoogleV2 { get; } = new Uri("https://dv.acme-v02.api.pki.goog/directory");
+
+        /// <summary>
+        /// Gets the URI for Google V2 staging server.
+        /// </summary>
+        /// <value>
+        /// The URI for Google V2 staging server.
+        /// </value>
+        public static Uri GoogleStagingV2 { get; } = new Uri("https://dv.acme-v02.test-api.pki.goog/directory");
+
+        /// <summary>
         /// Gets the URI for Let's Encrypt ACME v2 production server.
         /// </summary>
         /// <value>
@@ -22,5 +38,13 @@ namespace Certes.Acme
         /// The URI for Let's Encrypt V2 staging server.
         /// </value>
         public static Uri LetsEncryptStagingV2 { get; } = new Uri("https://acme-staging-v02.api.letsencrypt.org/directory");
+
+        /// <summary>
+        /// Gets the URI for ZeroSSL ACME v2 production server.
+        /// </summary>
+        /// <value>
+        /// The URI for ZeroSSL ACME v2 production server.
+        /// </value>
+        public static Uri ZeroSslV2 { get; } = new Uri("https://acme.zerossl.com/v2/DV90");
     }
 }
